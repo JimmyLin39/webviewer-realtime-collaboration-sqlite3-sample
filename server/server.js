72a8a08b79fdc4ce3dcb53507a6bb6c0bbc80917
Jimmy Lin <jimmylin0309@gmail.com>
2020-10-13 15:38:53 -0700

    setup collab-server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,25 @@
+const CollabServer = require('@pdftron/collab-server').default;
 const express = require('express');
+
 const app = express();
 const port = 3000;
-const annotationHandler = require('./annotationHandler');
 const bodyParser = require('body-parser');
 const open = require('open');
+const annotationHandler = require('./annotationHandler');
+
+const server = new CollabServer({
+  resolvers: {
+    Query: {},
+    Mutation: {},
+  },
+});
 
 app.use(bodyParser.text());
 app.use(express.static('client'));
-
+server.start(8000);
 app.listen(port, '0.0.0.0', () => {
   console.info(`Server listening at port ${port}`);
-  open('http://localhost:3000/index.html')
+  open('http://localhost:3000/index.html');
 });
 
-annotationHandler(app);
+// annotationHandler(app);
