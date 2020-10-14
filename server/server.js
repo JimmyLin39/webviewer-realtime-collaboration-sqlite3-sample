48fc2a714d83c06af9b1533c5abd43c805c3e8ce
Jimmy Lin <jimmylin0309@gmail.com>
2020-10-13 21:04:12 -0700

    inital apollo-collab setup

--- server/server.js
+++ server/server.js
@@ -1,25 +1,33 @@
 const CollabServer = require('@pdftron/collab-server').default;
 const express = require('express');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const open = require('open');
 const annotationHandler = require('./annotationHandler');
 
+const corsOption = {
+  origin: true,
+  credentials: true,
+};
 const server = new CollabServer({
   resolvers: {
     Query: {},
     Mutation: {},
   },
+  logLevel: CollabServer.LogLevels.INFO,
+  corsOption,
 });
 
 app.use(bodyParser.text());
-app.use(express.static('client'));
-server.start(8000);
-app.listen(port, '0.0.0.0', () => {
-  console.info(`Server listening at port ${port}`);
-  open('http://localhost:3000/index.html');
-});
+// app.use(express.static('client'));
+server.start(port);
+// app.listen(port, '0.0.0.0', () => {
+//   console.info(`Server listening at port ${port}`);
+//   open('http://localhost:3000/index.html');
+// });
 
 // annotationHandler(app);
